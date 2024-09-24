78d0e52b98c14bbb9ae2d845d64616bc9975cb95
ruslank <rkrishtalyan@gmail.com>
2024-09-25 02:21:28 +0300

    Homework 02_06 (added documentation)

--- Homeworks/HW_02/HW_02_06.py
+++ Homeworks/HW_02/HW_02_06.py
@@ -1,14 +1,33 @@
+"""Завдання 6: Калькулятор з використанням замикань
+
+Створити калькулятор, який використовує замикання для створення функцій додавання, віднімання, множення та ділення.
+
+1.	Написати функцію create_calculator, яка приймає оператор (наприклад, '+', '-', '*', '/')
+    та повертає функцію для виконання обчислень.
+2.	Використати цю функцію, щоб створити калькулятор для кількох операцій, і протестувати його.
+"""
+
+
 def create_calculator(operator):
+    """
+    Create and return calculation function based on provided operator.
+
+    Supports +, -, *, / operators only.
+    """
     def adding(a, b):
+        """Summarize two values."""
         return a + b
 
     def subtracting(a, b):
+        """Subtract one value from another."""
         return a - b
 
     def multiplying(a, b):
+        """Multiply one value by another."""
         return a * b
 
     def dividing(a, b):
+        """Divide one value by another. Return "Division by zero" if divisor = 0."""
         if b != 0:
             return a / b
         else:
@@ -20,7 +39,7 @@ def create_calculator(operator):
         return subtracting
     elif operator == "*":
         return multiplying
-    else:
+    elif operator == "/":
         return dividing
 
 
