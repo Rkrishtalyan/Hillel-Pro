4a872bb44a4799a9ab74912c60aa71b769a098dc
ruslank <rkrishtalyan@gmail.com>
2024-09-25 02:47:29 +0300

    Homework 02_08 (added documentation)

--- Homeworks/HW_02/HW_02_08.py
+++ Homeworks/HW_02/HW_02_08.py
@@ -1,4 +1,16 @@
+"""
+Завдання 8: Зберігання налаштувань користувача.
+
+Реалізувати систему зберігання налаштувань користувача за допомогою замикань.
+
+1.	Створити функцію create_user_settings, яка повертає функцію для зберігання і отримання налаштувань.
+2.	Налаштування можуть включати такі параметри, як theme, language і notifications.
+3.	Додати можливість зберігати, змінювати та переглядати налаштування.
+"""
+
+
 def create_user_settings(user_name):
+    """Create user_settings dict with default values. Return function to re-define, add new or view existing values."""
     user_settings = {
         "theme": None,
         "language": None,
@@ -6,6 +18,7 @@ def create_user_settings(user_name):
     }
 
     def save_setting(**kwargs):
+        """Update user_settings dict with defined values. Print current settings if no arguments provided."""
         if kwargs:
             for key, value in kwargs.items():
                 user_settings[key] = value
