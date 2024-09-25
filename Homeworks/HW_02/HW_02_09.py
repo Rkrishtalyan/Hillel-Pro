e8b4dc97b4e12e31a16d5fa6e187b924e50b4555
ruslank <rkrishtalyan@gmail.com>
2024-09-25 03:09:51 +0300

    Homework 02_09 (added documentation)

--- Homeworks/HW_02/HW_02_09.py
+++ Homeworks/HW_02/HW_02_09.py
@@ -1,10 +1,21 @@
+"""
+Завдання 9: Кешування результатів функції
+
+Написати програму для кешування результатів функції, щоб покращити продуктивність.
+
+1.	Створити функцію memoize, яка приймає функцію та повертає нову функцію, що зберігає результати викликів.
+2.	Використати цю функцію, щоб кешувати результати обчислень (наприклад, факторіал або фібоначі).
+"""
+
 from time import time
 
 
 def memoize(func):
+    """Return wrapper function with caching capability."""
     cache = {}
 
     def wrapper(*args):
+        """Store and return computed results. Compute, save and return new calculations."""
         if args in cache:
             return cache[args]
         result = func(*args)
@@ -15,6 +26,7 @@ def memoize(func):
 
 @memoize
 def fibonacci(n):
+    """Compute the nth Fibonacci number."""
     if n < 2:
         return n
     else:
@@ -23,6 +35,7 @@ def fibonacci(n):
 
 @memoize
 def factorial(n):
+    """Compute the factorial of n."""
     if n == 0:
         return 1
     return n * factorial(n - 1)
@@ -31,8 +44,10 @@ def factorial(n):
 fib_start = time()
 print(fibonacci(120))
 fib_end = time()
+
 fac_start = time()
 print(factorial(60))
 fac_end = time()
+
 print(fib_end - fib_start)
 print(fac_end - fac_start)
