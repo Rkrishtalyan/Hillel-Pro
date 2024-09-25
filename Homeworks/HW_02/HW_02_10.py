216687b3ccf6738b28cdc902a92a7d42e440cb22
ruslank <rkrishtalyan@gmail.com>
2024-09-25 03:28:56 +0300

    Homework 02_10 (added documentation)

--- Homeworks/HW_02/HW_02_10.py
+++ Homeworks/HW_02/HW_02_10.py
@@ -1,4 +1,15 @@
+"""
+Завдання 10: Створення товарів для онлайн-магазину.
+
+Розробити програму для управління товарами в онлайн-магазині, використовучи карирувані функції.
+
+1.	Написати функцію create_product, яка приймає назву, ціну та кількість товару.
+2.	Використати замикання для створення функції, яка дозволяє змінювати ціну товару.
+"""
+
+
 def create_product(name):
+    """Populate product details via currying function call. Return dict."""
     def set_price(sell_price):
         def set_quantity(qty):
             item = {
@@ -12,7 +23,9 @@ def create_product(name):
 
 
 def price_manager(item):
+    """Create and return price managing functions."""
     def update_price(new_price):
+        """Update product price. Print corresponding message."""
         nonlocal item
         item["price"] = new_price
         print(f'\nЦіна товару "{item["name"]}" оновлена до {item["price"]} грн.')
