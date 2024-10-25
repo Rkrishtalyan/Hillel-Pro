9a071211be25fe93136d62730568f1982d2f4b58
ruslank <rkrishtalyan@gmail.com>
2024-10-25 23:15:28 +0300

    Homework 11_02 upd

--- Homeworks/hw_11/hw_11_02.py
+++ Homeworks/hw_11/hw_11_02.py
@@ -15,21 +15,11 @@ import asyncio
 import aiohttp
 
 
-async def fetch_content(session, url):
-    """
-    Fetch content from the given URL using the provided session.
-
-    :param session: The aiohttp ClientSession object to make requests.
-    :type session: aiohttp.ClientSession
-    :param url: The URL to fetch content from.
-    :type url: str
-    :return: The content of the URL as a string.
-    :rtype: str
-    :raises: Raises an exception if the request fails.
-    """
-    async with session.get(url) as response:
-        response.raise_for_status()  # raises exception for 4xx and 5xx errors
-        return await response.text()
+async def fetch_content(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()  # raises exception for 4xx and 5xx errors
+            return await response.text()
 
 
 async def fetch_all(urls):
@@ -41,13 +31,12 @@ async def fetch_all(urls):
     :return: A list of results, which may include exceptions if fetching failed.
     :rtype: list
     """
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for url in urls:
-            task = asyncio.create_task(fetch_content(session, url))
-            tasks.append(task)
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        return results
+    tasks = []
+    for url in urls:
+        task = asyncio.create_task(fetch_content(url))
+        tasks.append(task)
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    return results
 
 
 # ---- Main Program Execution ----
