823a5be3bf8939d0f2beeb9c8a9c1bcf40a2e94b
ruslank <rkrishtalyan@gmail.com>
2024-10-27 22:43:12 +0200

    Homework 12_04

---
+++ Homeworks/hw_12/hw_12_04.py
@@ -0,0 +1,29 @@
+"""
+Завдання 4. Форматування дати.
+
+Напишіть функцію, яка перетворює дати з формату DD/MM/YYYY у формат YYYY-MM-DD.
+"""
+
+import re
+
+
+def convert_date(date):
+    """
+    Convert a date from MM/DD/YYYY format to YYYY-MM-DD format.
+
+    This function extracts the year, month, and day components from a date string
+    and formats them as 'YYYY-MM-DD'.
+
+    :param date: The date string in MM/DD/YYYY format.
+    :type date: str
+    :return: The reformatted date string in YYYY-MM-DD format.
+    :rtype: str
+    """
+    year = re.search(r'\d{4}', date)
+    month = re.search(r'\d{1,2}/(\d{1,2})', date)
+    day = re.search(r'(\d{1,2})/\d{1,2}', date)
+    return f'{year.group()}-{month.group(1).zfill(2)}-{day.group(1).zfill(2)}'
+
+
+# ---- Test date conversion function ----
+print(convert_date('27/10/2024'))
