46fc523fcd3cab8d8abce36e63bd6a78a2dedcc5
ruslank <rkrishtalyan@gmail.com>
2024-11-05 19:16:56 +0200

    Homework 14 + optional

--- Homeworks/hw_14/my_site/home/views.py
+++ Homeworks/hw_14/my_site/home/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
-# ---- View Functions for Main Pages ----
+# ---- View Functions for Each Page ----
 
 def home(request):
     """
@@ -10,69 +8,64 @@ def home(request):
 
     :param request: The HTTP request object.
     :type request: HttpRequest
-    :return: Rendered home page response.
+    :return: Rendered home page.
     :rtype: HttpResponse
     """
     return render(request, 'home/home.html')
 
-
 def about(request):
     """
-    Return a simple response for the 'About us' page.
+    Render the about page.
 
     :param request: The HTTP request object.
     :type request: HttpRequest
-    :return: 'About us' page response.
+    :return: Rendered about page.
     :rtype: HttpResponse
     """
-    return HttpResponse('"About us" page')
-
+    return render(request, 'home/about.html')
 
 def contact(request):
     """
-    Return a simple response for the contact page.
+    Render the contact page.
 
     :param request: The HTTP request object.
     :type request: HttpRequest
-    :return: Contact page response.
+    :return: Rendered contact page.
     :rtype: HttpResponse
     """
-    return HttpResponse('Contact us!')
-
-
-# ---- View Functions with URL Parameters ----
+    return render(request, 'home/contact.html')
 
 def post_view(request, id):
     """
-    Display a post based on its ID.
+    Render a post page with a specific post ID.
 
     :param request: The HTTP request object.
     :type request: HttpRequest
-    :param id: The ID of the post to view.
+    :param id: The ID of the post.
     :type id: int
-    :return: Response with the post number.
+    :return: Rendered post page with the given ID.
     :rtype: HttpResponse
     """
-    return HttpResponse(f'You view post number {id}')
-
+    context = {'id': id}
+    return render(request, 'home/post.html', context)
 
 def profile_view(request, username):
     """
-    Display the profile of a user based on the username.
+    Render a user profile page with a specific username.
 
     :param request: The HTTP request object.
     :type request: HttpRequest
-    :param username: The username of the profile to view.
+    :param username: The username of the profile.
     :type username: str
-    :return: Response with the profile information.
+    :return: Rendered profile page with the given username.
     :rtype: HttpResponse
     """
-    return HttpResponse(f'You view profile of user {username}')
-
+    context = {'username': username}
+    return render(request, 'home/profile.html', context)
 
 def event_view(request, year, month, day):
     """
-    Display an event based on the provided date.
+    Render an event page for a specific date.
 
     :param request: The HTTP request object.
     :type request: HttpRequest
@@ -82,7 +75,12 @@ def event_view(request, year, month, day):
     :type month: int
     :param day: The day of the event.
     :type day: int
-    :return: Response with the event date.
+    :return: Rendered event page with the specified date.
     :rtype: HttpResponse
     """
-    return HttpResponse(f'Event date: {year}-{month}-{day}')
+    context = {
+        'year': year,
+        'month': month,
+        'day': day
+    }
+    return render(request, 'home/event.html', context)
