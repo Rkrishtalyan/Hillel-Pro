44db8886223bf83141303fd3de49c4acf4e08b01
ruslank <rkrishtalyan@gmail.com>
2024-11-28 20:20:16 +0200

    Homework 21.01 (MySQL db)

--- Homeworks/hw_21/hello_world/hello_world/settings.py
+++ Homeworks/hw_21/hello_world/hello_world/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hello_world.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rkrishtalyan$default',
+        'USER': 'rkrishtalyan',
+        'PASSWORD': 'Curious, huh?',
+        'HOST': 'rkrishtalyan.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
