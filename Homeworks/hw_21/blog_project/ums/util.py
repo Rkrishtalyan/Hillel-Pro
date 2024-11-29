9eaaf0ed8e0787e09efb93765ff87e532a6422cc
ruslank <rkrishtalyan@gmail.com>
2024-11-29 19:26:11 +0200

    Homework 21. Adding bootstrap

--- Homeworks/hw_21/blog_project/ums/util.py
+++
@@ -1,6 +0,0 @@
-from ums.models import UserProfile
-from django.contrib.auth.models import User
-
-for user in User.objects.all():
-    if not hasattr(user, 'userprofile'):
-        UserProfile.objects.create(user=user)
