02580d8534dace9356e65b16e2ccdaae0424bc82
ruslank <rkrishtalyan@gmail.com>
2024-11-29 19:35:17 +0200

    Homework 21. Adjusting view designs

--- Homeworks/hw_21/blog_project/ums/models.py
+++ Homeworks/hw_21/blog_project/ums/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100, blank=True)
