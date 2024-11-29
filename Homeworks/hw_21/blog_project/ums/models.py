4962471a3132476c75e69ac011d3c1078f3078be
ruslank <rkrishtalyan@gmail.com>
2024-11-29 19:39:21 +0200

    Homework 21. Adjusting view designs

--- Homeworks/hw_21/blog_project/ums/models.py
+++ Homeworks/hw_21/blog_project/ums/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100, blank=True)
