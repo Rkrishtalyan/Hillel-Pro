cd2ccd0dc8877c2f2b5400dbe87cc455c8439d82
ruslank <rkrishtalyan@gmail.com>
2024-12-09 23:23:40 +0200

    Homework 23. Task 5

--- Homeworks/hw_23/customization_in_django/root/settings.py
+++ Homeworks/hw_23/customization_in_django/root/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'web_site.middleware.CustomHeaderMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
