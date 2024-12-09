9a8dce0677f305d0d9fc58597b8c8f87109ce79d
ruslank <rkrishtalyan@gmail.com>
2024-12-09 21:45:45 +0200

    Homework 23. Task 3

--- Homeworks/hw_23/customization_in_django/web_site/admin.py
+++ Homeworks/hw_23/customization_in_django/web_site/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Article, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+def mark_as_reviewed(modeladmin, request, queryset):
+    queryset.update(reviewed=True)
+
+mark_as_reviewed.short_description = "Mark selected articles as reviewed"
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'reviewed')
+    list_filter = ('category', 'reviewed')
+    actions = [mark_as_reviewed]
+    inlines = [CommentInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article', 'text', 'created_at')
+    list_filter = ('article', 'created_at')
--- Homeworks/hw_23/customization_in_django/web_site/models.py
+++ Homeworks/hw_23/customization_in_django/web_site/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Task 1
 
 class UpperCaseCharField(models.CharField):
@@ -11,11 +12,31 @@ class UpperCaseCharField(models.CharField):
 
 
 class Article(models.Model):
+    CATEGORY_CHOICES = [
+        ('tech', 'Technology'),
+        ('lifestyle', 'Lifestyle'),
+        ('news', 'News'),
+        ('other', 'Other'),
+    ]
+
     title = UpperCaseCharField(max_length=255)
     body = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='other')
+    reviewed = models.BooleanField(default=False)
 
     def count_words_in_title(self):
         return len(self.title.split())
 
     def __str__(self):
         return self.title
+
+
+# Task 3
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on {self.article.title}: {self.text[:30]}"
