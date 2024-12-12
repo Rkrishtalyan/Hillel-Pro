919119e98c9417ddbe08b98217fcc1eeda21c29e
rkrishtalyan <rkrishtalyan@gmail.com>
2024-12-12 18:55:46 +0200

    Homework 24, GraphQL ready

---
+++ Homeworks/hw_24/elasticsearch_and_graphql/test_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from elasticsearch_dsl import Document, Text, Date
+from elasticsearch_dsl.connections import connections
+
+connections.create_connection(hosts=['http://localhost:9201'])
+
+class DataDocument(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # class Index:
+    #     name = 'data_index'
+    #
+    # def save(self, **kwargs):
+    #     return super().save(**kwargs)
+
+    def __str__(self):
+        return self.title
---
+++ Homeworks/hw_24/elasticsearch_and_graphql/test_app/urls.py
@@ -0,0 +1,9 @@
+from django.template.defaulttags import csrf_token
+from django.urls import path
+from graphene_django.views import GraphQLView
+from test_app.schema import schema
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=schema))),
+]
