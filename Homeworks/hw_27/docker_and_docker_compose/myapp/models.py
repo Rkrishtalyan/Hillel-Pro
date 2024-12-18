d5f6d96ffdb8cae4df8647ba0dacc6fcefc99edb
ruslank <rkrishtalyan@gmail.com>
2024-12-19 00:01:23 +0200

    Homework 27. Part 1 (added test model)

--- Homeworks/hw_27/docker_and_docker_compose/myapp/models.py
+++ Homeworks/hw_27/docker_and_docker_compose/myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    """
+    Represent a test entry with a title and creation timestamp.
+
+    The Test model is a simple representation of a database entry with a
+    title and automatic timestamp.
+
+    :var title: The title of the entry.
+    :type title: str
+    :var created_at: The timestamp of when the entry was created.
+    :type created_at: datetime
+    """
+
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Return the string representation of the Test instance.
+
+        :return: The title of the entry.
+        :rtype: str
+        """
+        return self.title
